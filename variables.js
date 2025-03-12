1ae28611ba384cc1b1713c95a58488bf0e17e542
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-03-12 20:49:10 +0300

    declaring variables

---
+++ variables.js
@@ -0,0 +1,21 @@
+//declaring variables in js
+let firstName = "Bakari";
+let lastName = "Juma";
+let country = "Kenya";
+let city = "Nairobi";
+let age = 25;
+let isMarried = false;
+
+//declaring variables with number values
+
+const gravity = 9.81;
+const PI = 3.14;
+const boilingPoint = 100;
+
+//variables can also be declared in one line separated by comma
+
+let name = "Bakari",
+  job = "Software Developer",
+  live = "Nairobi";
+
+console.log(job);
