09aa14e82663aaa07b1598138f5c1843fb596b79
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-03-13 12:12:11 +0300

    more exercises

--- day 1 introduction and variables/Datatypes/booleanOperators/exercises.js
+++ day 1 introduction and variables/Datatypes/booleanOperators/exercises.js
@@ -9,7 +9,26 @@ let isSunny = false;
 
 console.log(!(4 > 3) && 10 < 12);
 
-let base = prompt("Enter the base");
+//ex1
+/*let base = prompt("Enter the base");
 let height = prompt("enter the height");
 let area = 0.5 * base * height;
-console.log(alert(`Your area is ${area}`));
+console.log(alert(`Your area is ${area}`));*/
+
+//ex2
+let sideA = parseInt(prompt("Enter side A"));
+let sideB = parseInt(prompt("Enter side B"));
+let sideC = parseInt(prompt("Enter side C"));
+let perimeter = sideA + sideB + sideC;
+console.log(alert(`The perimeter of the triangle is ${perimeter}`));
+
+//ex3
+let width = parseInt(prompt("Enter the width"));
+let length = parseInt(prompt("Enter the lenth"));
+let areaOfRectangle = length * width;
+let perimeterOfRectangle = 2 * (length + width);
+console.log(
+  alert(
+    `This is the area of a rectangle ${areaOfRectangle} while its perimeter is ${perimeterOfRectangle}`
+  )
+);
